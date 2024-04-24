4ef58fc7c39f519514ec05948c573ed84721a363
Ana Julia <anajuliadefendi@gmail.com>
2024-04-24 11:45:42 -0300

    contar palavras

--- script.js
+++ script.js
@@ -13,20 +13,23 @@ function handleImputTexto(evento){
         divContador.innerHTML = tamanho + " caracteres" 
        }
    } else{
-
+    let qtdepalavras = inputTexto.value.split(" ").length
+    if (qtdepalavras == 1){
+        divContador.innerHTML = qtdepalavras + " palavra" 
+       } else {
+        divContador.innerHTML = qtdepalavras + " palavras" 
+       }
    }
-   
-
 }
 
 function handleBtnContar(evento){
     console.log("click contar")
     if (modo === "caracteres"){
-        modo = "palavras "
-        btnContar.innerHTML = "Contar Palavras"
+        modo = "palavras"
+        btnContar.innerHTML = "Contar Caracteres"
     } else{
         modo = "caracteres"
-        btnContar.innerHTML = "Contar Caracteres"
+        btnContar.innerHTML = "Contar Palavras"
     }
 
 }
