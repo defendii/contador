c6fd3abb3458b325f1d4683a8688c36b528b2e2a
Ana Julia <anajuliadefendi@gmail.com>
2024-04-24 10:29:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-const inputTexto = document.querySelector("[input-texto]")
-const divContador = document.querySelector("[txt-contador]")
-const btnContar = document.querySelector("[btn-contar]")
+const inputTexto = document.querySelector("[input-texto]");
+const divContador = document.querySelector("[txt-contador]");
+const btnContar = document.querySelector("[btn-contar]");
 
 let modo = "caracteres"
 
-function handleInputTexto(){
-    console.log(inputTexto.value)
-
-    if (modo === "caracteres"){
-
-    } else{
-
-    }
-}
-
-function handleBtnContar(){
-    console.log("click Contar")
-    if (modo === "caracteres"){
-        modo = "palavras"
-        btnContar.innerText = "Contar Caracteres"
-    } else {
-        modo = "caracteres"
-        btnContar.innerText = "Contar Palavras"
-    }
-}
-
-inputTexto.addEventListener("input", handleInputTexto)
+inputTexto.addEventListener("input", handleImputTexto)
 
 btnContar.addEventListener("click", handleBtnContar)
+
+function handleImputTexto(evento){
+   let tamanho = inputTexto.value.length
+   console.log(tamanho) 
+
+}
+
+function handleBtnContar(evento){
+    console.log("click contar")
+    if (modo === "caracteres"){
+        modo = "palavras "
+        btnContar.innerHTML = "Contar Palavras"
+    } else{
+        modo = "caracteres"
+        btnContar.innerHTML = "Contar Caracteres"
+    }
+
+}
